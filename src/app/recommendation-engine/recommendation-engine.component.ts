aff13524d51e1dd3b7417a6f3875fbf4091b87c2
KeerthanaCM <keerthana.cm.01@gmail.com>
2022-12-01 14:49:14 +0530

    reccomendation engine UI

---
+++ src/app/recommendation-engine/recommendation-engine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recommendation-engine',
+  templateUrl: './recommendation-engine.component.html',
+  styleUrls: ['./recommendation-engine.component.css']
+})
+export class RecommendationEngineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
