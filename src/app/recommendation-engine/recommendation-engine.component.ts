7d29bd6ac8fe2a60dfd134b41163a6202fac0523
Dhinesh Venkitasamy <Dhinesh.Venkitasamy2@gmail.com>
2022-12-01 15:07:03 +0530

    form added

--- src/app/recommendation-engine/recommendation-engine.component.ts
+++ src/app/recommendation-engine/recommendation-engine.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recommendation-engine.component.css']
 })
 export class RecommendationEngineComponent implements OnInit {
-
+  model: any = {"inputType":"","gain":""};
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model))
+  }
 }
