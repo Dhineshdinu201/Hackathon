7e75064d24e047fb4842e147752a4cd6cf5e7320
Dhinesh Venkitasamy <Dhinesh.Venkitasamy2@gmail.com>
2022-12-02 15:30:36 +0530

    initial

--- src/app/recommendation-engine/recommendation-engine.component.ts
+++ src/app/recommendation-engine/recommendation-engine.component.ts
@@ -7,14 +7,19 @@ import { HttpApiService } from '../http-api.service';
   styleUrls: ['./recommendation-engine.component.css']
 })
 export class RecommendationEngineComponent implements OnInit {
-  model: any = { "inputType": "", "gain": 0, "userId": "1" };
+  model: any = { "inputType": "", "gain": "0", "userId": "1" };
   result: any;
   constructor(private httpApiService: HttpApiService) { }
   ngOnInit(): void {
   }
 
   async onSubmit() {
-    this.result = await this.httpApiService.post("api/Hackathon", this.model).toPromise()
+    this.result = await this.httpApiService.post("api/Hackathon/GainRecommend", this.model).toPromise();
 
+  }
+  async recommentHpf(gain: any) {
+    this.model["HPF"] = "0";
+    this.model["gain"] = gain;
+    this.result = await this.httpApiService.post("api/Hackathon/HpfRecommend", this.model).toPromise();
   }
 }
