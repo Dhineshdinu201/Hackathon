bc9af34f9b493a9856237ed8ecae48ddbe0eaf3a
Dhinesh Venkitasamy <Dhinesh.Venkitasamy2@gmail.com>
2022-12-05 11:31:08 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecommendationEngineComponent } from './recommendation-engine/recommendation-engine.component';
 import { RecommendationEngine2Component } from './recommendation-engine2/recommendation-engine2.component';
 
 const routes: Routes = [
   {
     path: "re2", component: RecommendationEngine2Component
+  },
+   {
+    path: "re", component: RecommendationEngineComponent
   }
 ];
 
