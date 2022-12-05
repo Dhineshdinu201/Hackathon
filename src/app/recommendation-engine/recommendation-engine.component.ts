ade4ec6605cb66e6525ce3ccb99af8d281ec3bab
Dhinesh Venkitasamy <Dhinesh.Venkitasamy2@gmail.com>
2022-12-05 11:39:12 +0530

    changes

--- src/app/recommendation-engine/recommendation-engine.component.ts
+++ src/app/recommendation-engine/recommendation-engine.component.ts
@@ -9,17 +9,25 @@ import { HttpApiService } from '../http-api.service';
 export class RecommendationEngineComponent implements OnInit {
   model: any = { "inputType": "", "gain": "0", "userId": "1" };
   result: any;
+  showResult: boolean = false;
+  showButton: boolean = true;
   constructor(private httpApiService: HttpApiService) { }
   ngOnInit(): void {
   }
 
   async onSubmit() {
+    
     this.result = await this.httpApiService.post("api/Hackathon/GainRecommend", this.model).toPromise();
-
+    this.showResult = true;
+    this.showButton = true;
   }
   async recommentHpf(gain: any) {
+    this.showButton = false;
     this.model["HPF"] = "0";
     this.model["gain"] = gain;
     this.result = await this.httpApiService.post("api/Hackathon/HpfRecommend", this.model).toPromise();
+  }
+  onInputTypeChange() {
+    this.showResult = false;
   }
 }
